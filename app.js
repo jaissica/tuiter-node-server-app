6cd6d5b38a5eb5b3bd7d3223dcaa47edb919751f
jaissica <jaissica.hora@gmail.com>
2023-06-13 17:40:40 -0400

    Render Commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
 app.use(
     cors({
       credentials: true,
-     origin : "http://localhost:3000",
+     origin : "https://tuiter-node-server-app-sdjx.onrender.com",
     })
    );
    
