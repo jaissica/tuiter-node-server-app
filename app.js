2d6508cce267b9b5c69a08581a76dac873cc788b
jaissica <jaissica.hora@gmail.com>
2023-06-20 01:05:08 -0400

    Hopefully Final 7

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const app = express();
 import mongoose from "mongoose";
 
 //const CONNECTION_STRING = "mongodb+srv://dhyeysavaliya2000:dhyey123@cluster0.exksxhm.mongodb.net/users?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
- const CONNECTION_STRING = "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/tuiter?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
-//const CONNECTION_STRING = "mongodb+srv://jaissica:Mongo1201@cluster0.pinshqk.mongodb.net/tuiter?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
+//  const CONNECTION_STRING = "mongodb+srv://umangthakuremail:Mongo1511@cluster0.ghe9flu.mongodb.net/tuiter?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
+const CONNECTION_STRING = "mongodb+srv://jaissica:Mongo1201@cluster0.e7eciga.mongodb.net/tuiter?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/tuiter'; 
 // const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'; 
 
 console.log(process.env.DB_CONNECTION_STRING);
